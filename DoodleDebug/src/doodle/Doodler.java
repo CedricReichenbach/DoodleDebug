5cf73231522ef045bfd4b220c1cc59329b745b4d
Cedric Reichenbach <didu@gmx.ch>
2011-10-18 09:24:13 +0200

    working on skeletton

--- DoodleDebug/src/doodle/Doodler.java
+++ DoodleDebug/src/doodle/Doodler.java
@@ -1,5 +1,21 @@
 package doodle;
 
 public class Doodler {
-	// TODO
+	
+	/**
+	 * Visualizes any Object, either using draw Method of the object itself (if existing) or
+	 * does a default drawing.
+	 * @param Object o
+	 */
+	public void draw(Object o) {
+		// TODO: Use os draw method, if existing or else, use drawDefault
+	}
+	
+	/**
+	 * Makes a default drawing for object that don't implement their own draw method.
+	 * @param Object o
+	 */
+	private void drawDefault(Object o) {
+		
+	}
 }
