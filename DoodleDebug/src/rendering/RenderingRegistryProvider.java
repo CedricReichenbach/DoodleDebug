148025ab4aab6af27bf6a2a0cb391f825c5a9163
Cedric Reichenbach <didu@gmx.ch>
2011-10-27 15:17:54 +0200

    formatting

--- DoodleDebug/src/rendering/RenderingRegistryProvider.java
+++ DoodleDebug/src/rendering/RenderingRegistryProvider.java
@@ -10,22 +10,19 @@ import plugins.Plugin;
 
 public class RenderingRegistryProvider implements Provider<RenderingRegistry> {
 
-
 	@Inject
 	Set<Plugin> allPlugins;
-	
-	
-	
+
 	@Override
 	public RenderingRegistry get() {
-		HashMap<Class<?>,Plugin> m = new HashMap<Class<?>, Plugin>();
-		for(Plugin p : allPlugins) {
-			for(Class<?> type : p.getDrawableClasses()) {
+		HashMap<Class<?>, Plugin> m = new HashMap<Class<?>, Plugin>();
+		for (Plugin p : allPlugins) {
+			for (Class<?> type : p.getDrawableClasses()) {
 				m.put(type, p);
 			}
 		}
 		RenderingRegistry registry = new RenderingRegistry(m);
-		
+
 		return registry;
 	}
 
