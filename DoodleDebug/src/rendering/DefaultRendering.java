e80c1fb7a4f9e4432ae9088351929b07f2f8e407
Cedric Reichenbach <didu@gmx.ch>
2011-10-31 18:17:56 +0100

    added Class DoodleCanvas for abstract virtual drawing with geometrical objects

--- DoodleDebug/src/rendering/DefaultRendering.java
+++ DoodleDebug/src/rendering/DefaultRendering.java
@@ -1,13 +1,23 @@
 package rendering;
 
+import view.DoodleCanvas;
+
+import doodle.Doodler;
 import doodle.Scratch;
 
 public class DefaultRendering implements Rendering {
 
 	@Override
-	public void render(Object o, Scratch top) {
+	public void render(Object o, DoodleCanvas c, Doodler d) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
+	@Override
+	public void render(Scratch top, DoodleCanvas c, Doodler d) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
--- DoodleDebug/src/rendering/Rendering.java
+++ DoodleDebug/src/rendering/Rendering.java
@@ -1,12 +1,30 @@
 package rendering;
 
+import view.DoodleCanvas;
+
+import doodle.Doodler;
 import doodle.Scratch;
 
 /**
  * Contains information of how an object should be rendered
+ * 
  * @author Cedric Reichenbach
- *
+ * 
  */
 public interface Rendering {
-	public void render(Object o, Scratch top);
+	/**
+	 * for Objects with no user information about desired rendering structure
+	 * 
+	 * @param Object
+	 *            o
+	 */
+	public void render(Object o, DoodleCanvas c, Doodler d);
+
+	/**
+	 * for Objects with user-given information about desired rendering structure
+	 * 
+	 * @param Scratch
+	 *            top
+	 */
+	public void render(Scratch top, DoodleCanvas c, Doodler d);
 }
