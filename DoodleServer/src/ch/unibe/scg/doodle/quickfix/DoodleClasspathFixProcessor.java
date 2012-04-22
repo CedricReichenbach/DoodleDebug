226bb2906e221964579be26f6ea8983d663a4ea7
Cedric Reichenbach <didu@gmx.ch>
2012-04-22 17:06:59 +0200

    working on classpathFix

---
+++ DoodleServer/src/ch/unibe/scg/doodle/quickfix/DoodleClasspathFixProcessor.java
@@ -0,0 +1,20 @@
+package ch.unibe.scg.doodle.quickfix;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.ui.text.java.ClasspathFixProcessor;
+
+public class DoodleClasspathFixProcessor extends ClasspathFixProcessor {
+
+	public DoodleClasspathFixProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ClasspathFixProposal[] getFixImportProposals(IJavaProject project,
+			String name) throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
