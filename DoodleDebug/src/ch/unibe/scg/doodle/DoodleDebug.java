3f26389e60d31b4dde5a9ddac7bc6e5d6a4ff20f
Cedric Reichenbach <didu@gmx.ch>
2012-07-26 18:56:55 +0200

    marked includeSystemOut() as deprecated: probably too complicated to implement with sense, but not rejected yet...

--- DoodleDebug/src/ch/unibe/scg/doodle/DoodleDebug.java
+++ DoodleDebug/src/ch/unibe/scg/doodle/DoodleDebug.java
@@ -9,17 +9,21 @@ package ch.unibe.scg.doodle;
  */
 public class DoodleDebug {
 	/**
-	 * Clear the output tab and leave a blank page. Suggestion: Use this method very carefully since
-	 * the history is cleared anyway at the beginning of each run.
+	 * Clear the output tab and leave a blank page. Suggestion: Use this method
+	 * very carefully since the history is cleared anyway at the beginning of
+	 * each run.
 	 */
 	public static void clearHistory() {
 		DoodleClient.instance().clearOutput();
 	}
-	
+
 	/**
 	 * Includes all Console inputs into DoodleDebug output
+	 * 
+	 * @deprecated WARNING: Not implemented yet!
 	 */
 	public static void includeSystemOut() {
-		// TODO
+		// XXX Problem: To catch everything, you have to implement an
+		// OutputStream which gets each character one after another...
 	}
 }
---
+++ DoodleDebug/src/ch/unibe/scg/doodle/stream/DoodleStream.java
@@ -0,0 +1,13 @@
+package ch.unibe.scg.doodle.stream;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class DoodleStream extends OutputStream {
+
+	@Override
+	public void write(int b) throws IOException {
+
+	}
+
+}
