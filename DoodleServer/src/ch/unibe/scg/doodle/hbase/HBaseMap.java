2013671dff7b615262e54c4283ad4f2b931edbab
Cedric Reichenbach <cedric.reichenbach@students.unibe.ch>
2013-12-29 16:39:47 +0100

    Added libraries and started writing code for HBase connection

--- DoodleServer/src/ch/unibe/scg/doodle/hbase/HBaseMap.java
+++ DoodleServer/src/ch/unibe/scg/doodle/hbase/HBaseMap.java
@@ -1,19 +1,55 @@
 package ch.unibe.scg.doodle.hbase;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
 public class HBaseMap implements Map<Integer, Object> {
 
+	private static final String TABLE_NAME = "objects";
+	private static final String ID_COL_TITLE = "id";
+	private static final String OBJECT_COL_TITLE = "object";
+
+	private final HBaseAdmin hbaseAdmin;
+
 	public HBaseMap() {
-		
+		Configuration hbaseConfiguration = HBaseConfiguration.create();
+		try {
+			System.out.println("Connecting to HBase...");
+			hbaseAdmin = new HBaseAdmin(hbaseConfiguration);
+			System.out.println("Connection to HBase established.");
+
+			assureTableExistence();
+		} catch (IOException e) {
+			System.err.println("Connection to HBase failed!");
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void assureTableExistence() throws IOException {
+		if (hbaseAdmin.tableExists(TABLE_NAME))
+			return;
+
+		HTableDescriptor tableDescriptor = new HTableDescriptor(
+				TableName.valueOf(TABLE_NAME)); // XXX: That's weird...
+		tableDescriptor.addFamily(new HColumnDescriptor(ID_COL_TITLE));
+		tableDescriptor.addFamily(new HColumnDescriptor(OBJECT_COL_TITLE));
+
+		hbaseAdmin.createTable(tableDescriptor);
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -61,7 +97,7 @@ public class HBaseMap implements Map<Integer, Object> {
 	@Override
 	public void putAll(Map<? extends Integer, ? extends Object> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
