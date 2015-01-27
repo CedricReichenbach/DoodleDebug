78fa4eb7f344279229f69118a7ba6e5f6b9bf026
Cedric Reichenbach <cedric.reichenbach@students.unibe.ch>
2015-01-27 20:05:47 +0100

    Introduce caching of database maps

--- DoodleDebug_Producer/src/ch/unibe/scg/doodle/database/DoodleDatabaseMapProvider.java
+++ DoodleDebug_Producer/src/ch/unibe/scg/doodle/database/DoodleDatabaseMapProvider.java
@@ -1,6 +1,8 @@
 package ch.unibe.scg.doodle.database;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.unibe.scg.doodle.database.hbase.HBaseStringMap;
 
@@ -9,18 +11,26 @@ public class DoodleDatabaseMapProvider {
 	@SuppressWarnings("rawtypes")
 	// eclipse/compiler is not smart enough
 	private static Class<? extends DoodleDatabaseMap> mapClass = HBaseStringMap.class;
+	private static Map<String, DoodleDatabaseMap<?>> mapCache = new HashMap<>();
 
 	Class<?> x = DoodleDatabaseMap.class;
 
 	@SuppressWarnings("rawtypes")
 	public static void setClass(Class<? extends DoodleDatabaseMap> newMapClass) {
 		mapClass = newMapClass;
+		mapCache = new HashMap<>(); // clear cache
 	}
 
 	@SuppressWarnings("unchecked")
 	public static <T> DoodleDatabaseMap<T> get(String mapName) {
+		if (mapCache.containsKey(mapName))
+			return (DoodleDatabaseMap<T>) mapCache.get(mapName);
+
 		try {
-			return mapClass.getConstructor(String.class).newInstance(mapName);
+			DoodleDatabaseMap<T> map = mapClass.getConstructor(String.class)
+					.newInstance(mapName);
+			mapCache.put(mapName, map);
+			return map;
 		} catch (InstantiationException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException e) {
