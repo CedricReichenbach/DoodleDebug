deda9517f7b4b26b0fad08b088da22a845795d26
Cedric Reichenbach <cedric.reichenbach@students.unibe.ch>
2015-06-16 17:25:10 +0200

    Start webapp server async

--- DoodleDebugWebclient/src/ch/unibe/scg/doodle/DoodleDebugWebapp.java
+++ DoodleDebugWebclient/src/ch/unibe/scg/doodle/DoodleDebugWebapp.java
@@ -15,11 +15,15 @@ public class DoodleDebugWebapp {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length > 0) {
-			if ("--help".equals(args[0])) {
+			String firstArg = args[0];
+			if ("--help".equals(firstArg)) {
 				printHelp();
 				return;
+			} else if ("stop".equals(firstArg)) {
+				stopServer();
+				return;
 			}
-			startServer(Integer.parseInt(args[0]));
+			startServer(Integer.parseInt(firstArg));
 		} else
 			startServer();
 	}
@@ -28,38 +32,58 @@ public class DoodleDebugWebapp {
 		System.out.println("~~ DoodleDebug web application ~~");
 		System.out.println("With no arguments: Start server on default port ("
 				+ DEFAULT_PORT + ")");
-		System.out.println("With one argument: Argument is server port");
+		System.out
+				.println("With one argument: Argument is either \"stop\" or a number as server port");
 		System.out
 				.println("For further configuration, e.g. a custom database connection, "
 						+ "you need to embed this application into another and do any configuration "
 						+ "before calling DoodleDebugWebapp.startServer([port]). More info: http://scg.unibe.ch/wiki/projects/DoodleDebug");
 	}
 
-	public static void startServer() throws Exception {
+	public static void startServer() {
 		startServer(DEFAULT_PORT);
 	}
 
-	public static void startServer(int port) throws Exception {
-		server = new Server(port);
-
-		webSocketHandler = new WebSocketHandler() {
+	public static void startServer(final int port) {
+		new Thread(new Runnable() {
 			@Override
-			public void configure(WebSocketServletFactory factory) {
-				factory.getPolicy().setIdleTimeout(600000);
-				factory.register(DoodleSocket.class);
-			}
-		};
-		webSocketHandler.setHandler(new WebsiteHandler());
+			public void run() {
+				server = new Server(port);
 
-		server.setHandler(webSocketHandler);
-		server.start();
-		System.out.println("DoodleDebug server running at http://localhost:"
-				+ port);
-		server.join();
+				webSocketHandler = new WebSocketHandler() {
+					@Override
+					public void configure(WebSocketServletFactory factory) {
+						factory.getPolicy().setIdleTimeout(600000);
+						factory.register(DoodleSocket.class);
+					}
+				};
+				webSocketHandler.setHandler(new WebsiteHandler());
+
+				server.setHandler(webSocketHandler);
+				try {
+					server.start();
+					System.out
+							.println("DoodleDebug server running at http://localhost:"
+									+ port);
+					server.join();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
-	public static void stopServer() throws Exception {
-		server.stop();
+
+	public static void stopServer() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					server.stop();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
